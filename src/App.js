1d60d563fe056253b37c50287e94b155e1c95e37
Lilly Ohr <lillyohr@gmail.com>
2023-01-19 20:12:49 -0500

    Adding experience explanation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import Navbar from "./components/Navbar"
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <NavBar/>
+        <Navbar/>
       </header>
       <main>
         <About/>
@@ -24,59 +25,13 @@ function App() {
   );
 }
 
-
-function NavBar(){
-  return(
-    <nav className="navbar">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a href="#" className="nav-link">
-            <span className="link-text home">Lilly Ohr</span>
-          </a>
-        </li>
-        {/* <li className="nav-item">
-          <a href="#about" className="nav-link">
-            <span className="link-text">About</span>
-          </a>
-        </li> */}
-        <li className="nav-item">
-          <a href="#skills" className="nav-link">
-            <span className="link-text">Skills</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#projects" className="nav-link">
-            <span className="link-text">Projects</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#experience" className="nav-link">
-            <span className="link-text">Experience</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#" className="nav-link">
-            <span className="link-text">Blog</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#" className="nav-link">
-            <span className="link-text">Resume</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  )
-}
-
 function About(){
   return(
     <section className='about' id="about">
       <h2 className="title-h2">About</h2>
       <section className="about-inner">
         <p>
-          I'm Lilly Ohr, a junior at Boston College majoring in computer science and linguistics. I'm interested in backend development and machine learning.
-          Scalable solutions with tech helping people.
+          Hello! I'm Lilly Ohr, a junior at Boston College majoring in computer science and linguistics. <br/> I'm interested in backend development and machine learning. 
         </p>
         <a href="https://www.linkedin.com/in/lillyohr/" target="_blank"><FontAwesomeIcon icon={ faLinkedin } /></a>
         <a href="https://github.com/ljohr" target="_blank"><FontAwesomeIcon icon={ faGithub } /></a>
@@ -93,7 +48,7 @@ function Skills(){
         <h4>Backend</h4>
         <ul>
           <li>Python, Java, C</li>
-          <li>Flask</li>
+          <li>Node JS, Flask</li>
         </ul>
       </section>
       <section className="skills-inner">
@@ -123,23 +78,25 @@ function Experience(){
         <div className="experience-info">
           <h4>Boston College</h4>
           <p className="description">Computer Systems TA</p>
+          <p className="location">Chestnut Hill, MA</p>
           <p className="date">Jan 2023 - Present</p>
         </div>
           <ul className="experience-description">
-            <li>Incoming TA for Spring 2023</li>
-            <li>To hold weekly office hours explaining concepts and helping with assignments</li>
-            <li>To grade weekly assignments</li>
-            <li>All assignments and course materials conducted in C</li>
+            <li>Holding 2-hour long office hours every week</li> 
+            <li>Helping students understand concepts</li>
+            <li>Helping students debug assignments</li>
+            <li>Grading weekly assignments</li>
           </ul>
       </section>
       <section className="experience-inner">
         <div className="experience-info">
           <h4>NASA</h4>
           <p className="description">L'SPACE NPWEE Subteam Lead</p>
+          <p className="location">Remote</p>
           <p className="date">May 2022 - Aug 2022</p>
         </div>
           <ul className="experience-description">
-            <li>Lead weekly business team meetings</li>
+            <li>Led weekly business team meetings</li>
             <li>Assigned tasks, lead 3 person subteam to create 7 page New Technology Report (NTR) detailing a possible improvement to existing NASA technology</li>
             <li><a className="link" href="https://drive.google.com/file/d/1e3RVvkMlDfaNZAuxrCcoiACa9rSUfLKF/view" target="_blank">
               New Technology Report 
@@ -150,10 +107,11 @@ function Experience(){
         <div className="experience-info">
           <h4>NASA</h4>
           <p className="description">L'SPACE MCA Project Manager</p>
+          <p className="location">Remote</p>
           <p className="date">Aug 2021 - Dec 2021</p>
         </div>
           <ul className="experience-description">
-            <li>Lead weekly general team meetings</li>
+            <li>Led weekly general team meetings</li>
             <li>Assigned tasks, moderated between subteams throughout 5 months</li>
             <li>Lead 9 person team to create 150 page Preliminary Design Review (PDR) detailing a hypothetical mission to Mars</li>
             <li><a className="link" href="https://drive.google.com/file/d/1bIIMl1pefHiWhEZNvwsxDlg36LiUmsmx/view" target="_blank">
