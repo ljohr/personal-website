e3ec725a002782dc24aa915395910fa4fc2349a1
Lilly Ohr <lillyohr@gmail.com>
2023-01-27 21:16:37 -0500

    setup gh-pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,6 @@ const Navbar = () => {
             <span className="link-text home">Lilly Ohr</span>
           </a>
         </li>
-        {/* <li className="nav-item">
-          <a href="#about" className="nav-link">
-            <span className="link-text">About</span>
-          </a>
-        </li> */}
         <li className="nav-item">
           <a href="#skills" className="nav-link">
             <span className="link-text">Skills</span>
@@ -27,11 +22,6 @@ const Navbar = () => {
         <li className="nav-item">
           <a href="#experience" className="nav-link">
             <span className="link-text">Experience</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#" className="nav-link">
-            <span className="link-text">Blog</span>
           </a>
         </li>
         <li className="nav-item">
